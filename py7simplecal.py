b75e925de2ed1a2bf4be6715b4995b914a9dcbef
Nikita Dandgavhal <146940801+Csbs23@users.noreply.github.com>
2023-12-13 21:27:51 +0530

    Add files via upload

---
+++ py7simplecal.py
@@ -0,0 +1,12 @@
+num1 =int(input("enter the first no: "))
+num2 =int(input("enter the second no: "))
+op = input("enter the operator: ")
+
+if op =='+':
+    print("The addtion is ",num1+num2)
+elif op =='-':
+    print("The subtraction is ",num1-num2)
+elif op =='*':
+    print("The multiplication is ",num1*num2)
+elif op =='/':
+    print("The division is",num1/num2)
